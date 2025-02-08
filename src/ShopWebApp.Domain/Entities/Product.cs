1cac27b606f7eaf31839208ed7472880ed8aaad7
Gorodnichev Max <shlikovmax2281337@mail.ru>
2025-02-08 09:20:35 +0300

    added product class

---
+++ src/ShopWebApp.Domain/Entities/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopWebApp.Domain.Entities
+{
+    /// <summary>
+    /// Product class
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// Product Id
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Product description
+        /// </summary>
+        public string Definition { get; set; }
+
+        /// <summary>
+        /// Product name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Product price
+        /// </summary>
+        public string Price { get; set; }
+
+        /// <summary>
+        /// Product image
+        /// </summary>
+        public string Image { get; set; }
+    }
+}
